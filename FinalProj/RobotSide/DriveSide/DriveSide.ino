405cdbda7668abb3287f6bf12fd31567bfb05be5
Shiv Dalla <sdalla@seas.upenn.edu>
2018-12-01 15:36:01 -0500

    added in servo stuff

--- FinalProj/RobotSide/DriveSide/DriveSide.ino
+++ FinalProj/RobotSide/DriveSide/DriveSide.ino
@@ -18,6 +18,9 @@
 #define EN3   25
 #define H3A   27  
 #define H4A   26
+#define BASE_SERVO 17 //TODO ADD PINS HERE
+#define ARM_SERVO 16 //TODO ADD PINS HERE
+
 
 // Wifi
 #define LOCALPORT       2800
@@ -30,12 +33,18 @@ byte dc_left;
 byte dc_right;
 byte dir_left;
 byte dir_right;
+byte base_pos;
+byte arm_pos;
 
+//Intialize Team Boolean, true is blue, false is red
+int teamIsBlue; 
 
 // For setting up ledcChannel
 byte resolution = 8;
 byte left_channel = 1;
 byte right_channel = 2;
+byte base_channel = 3;
+byte arm_channel = 4;
 int freq = 200;
 
 // WiFi
@@ -65,11 +74,17 @@ void setup() {
   pinMode(H3A, OUTPUT);
   pinMode(H4A, OUTPUT);
 
-  // PWM
+  // PWM for motors
   ledcSetup(left_channel, freq, resolution);
   ledcSetup(right_channel, freq, resolution);
   ledcAttachPin(EN1, left_channel);
   ledcAttachPin(EN3, right_channel);
+
+  // PWM for servos
+  ledcSetup(base_channel, freq, resolution);
+  ledcSetup(arm_channel, freq, resolution);
+  ledcAttachPin(BASE_SERVO, base_channel);
+  ledcAttachPin(ARM_SERVO, arm_channel);
 
   // WiFi
   WiFi.config(myIPaddress, IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
@@ -110,13 +125,19 @@ void readPacket () {
     udp.read(packetBuffer, UDP_PACKET_SIZE);
 
     // Parse the first to bytes into duty cycles
+    // motors
     dc_right = packetBuffer[0];
     dc_left = packetBuffer[1];
-
+    // servos
+    base_pos = packetBuffer[3];
+    arm_pos = packetBuffer[4];
+    
     // Parse the directions from the third byte
     byte dir = packetBuffer[2];
     dir_right = bitRead(dir, 1);
     dir_left = bitRead(dir,2);
+    teamIsBlue = bitRead(dir,3);
+    
 
   }
   // No packet received, sad.
@@ -128,7 +149,7 @@ void readPacket () {
 
 /*******************************************************
  * Function: controlMotors
- * Controls duty cycle, direction of PWM to each motor
+ * Controls duty cycle, direction of PWM to each motor and servo
  *******************************************************/
 void controlMotors () {
 
@@ -151,4 +172,6 @@ void controlMotors () {
   // Generate PWM signal based on supplied duty cycle
   ledcWrite(left_channel, dc_left);
   ledcWrite(right_channel, dc_right);
+  ledcWrite(base_channel, base_pos);
+  ledcWrite(arm_channel, arm_pos);
 }
